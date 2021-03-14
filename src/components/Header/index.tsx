3af67b84eb6be9f495919fc35678a5741a2b5e0c
Esteban Ciudad <eciudadconde@gmail.com>
2021-03-14 01:41:02 -0500

    add header layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,36 @@
+import { AppBar, Avatar, Box, Toolbar, Typography } from '@material-ui/core'
+import styled from 'styled-components'
+import logo from '../../assets/logo.png'
+
+const LogoContainer = styled.div`
+  flex-grow: 1;
+`
+
+const StyledAvatar = styled(Avatar)`
+  background-color: purple;
+  margin-left: 1rem;
+`
+
+const StyledLogo = styled.img`
+  height: 80px;
+`
+
 function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
-  );
+    <AppBar>
+      <Toolbar>
+        <LogoContainer>
+          <a href="/">
+            <StyledLogo src={logo} />
+          </a>
+        </LogoContainer>
+        <Box display="flex" alignItems="center">
+          <Typography>Balance: $10.00</Typography>
+          <StyledAvatar>EC</StyledAvatar>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
 }
 
-export default Header;
+export default Header
